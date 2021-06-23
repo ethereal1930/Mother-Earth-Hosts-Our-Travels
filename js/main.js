2a46cb3506f3cfea63098d6557628120a856bf4d
ethereal1930 <flashyeezy@gmail.com>
2021-06-23 09:02:04 +0300

    end

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-let swiper = new Swiper('.swiper-container', {
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    loop: true,
-    spaceBetween: 100,
+const swiper1 = document.querySelector('.slider-container'),
+		burger = document.querySelector('.burger'),
+		close = document.querySelector('.menu__close'),
+		menu = document.querySelector('.menu');
+        
+burger.addEventListener('click', () => {
+	menu.classList.add('menu--visible');
 });
 
+close.addEventListener('click', () => {
+	menu.classList.remove('menu--visible');
+});
